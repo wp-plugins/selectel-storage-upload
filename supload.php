84dc94330b191f131bbe0fe5afe308b55a2e748e
Mauhem <Mauhem@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-21 16:37:25 +0000

    Исправлена проблема с синхронизацией миниатюр, если было выбрано удаление файлов. Добавлена возможность синхронизации файлов по маске. Теперь можно просмотреть список файлов на ручную синхронизацию.
    git-svn-id: https://plugins.svn.wordpress.org/selectel-storage-upload/tags/1.3.0@1117573 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ supload.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Plugin Name: Selectel Storage Upload
+ * Plugin URI: http://wm-talk.net/supload-wordpress-plagin-dlya-zagruzki-na-selectel
+ * Description: The plugin allows you to upload files from the library to Selectel Storage
+ * Version: 1.3.0
+ * Author: Mauhem
+ * Author URI: http://wm-talk.net/
+ * License: GNU GPLv2
+ * Text Domain: selupload
+ * Domain Path: /lang
+
+ */
+load_plugin_textdomain('selupload', false, dirname(plugin_basename(__FILE__)) . '/lang');
+
+function selupload_incompatibile($msg)
+{
+    require_once ABSPATH . DIRECTORY_SEPARATOR . 'wp-admin' . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'plugin.php';
+    deactivate_plugins(__FILE__);
+    wp_die($msg);
+}
+
+if (is_admin() && (!defined('DOING_AJAX') || !DOING_AJAX)) {
+    if (version_compare(PHP_VERSION, '5.3.3', '<')) {
+        selupload_incompatibile(
+            __(
+                'Plugin Selectel Cloud Uploader requires PHP 5.3.3 or higher. The plugin has now disabled itself.',
+                'selupload'
+            )
+        );
+    } elseif (!function_exists('curl_version')
+        || !($curl = curl_version()) || empty($curl['version']) || empty($curl['features'])
+        || version_compare($curl['version'], '7.16.2', '<')
+    ) {
+        selupload_incompatibile(
+            __('Plugin Selectel Cloud Uploader requires cURL 7.16.2+. The plugin has now disabled itself.', 'selupload')
+        );
+    } elseif (!($curl['features'] & CURL_VERSION_SSL)) {
+        selupload_incompatibile(
+            __(
+                'Plugin Selectel Cloud Uploader requires that cURL is compiled with OpenSSL. The plugin has now disabled itself.',
+                'selupload'
+            )
+        );
+    }
+}
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'code.php';
---
+++ vendor/endeveit/open-stack-storage/src/OpenStackStorage/Authentication.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Nikita Vershinin <endeveit@gmail.com>
+ * @license MIT
+ */
+namespace OpenStackStorage;
+
+/**
+ * Authentication instances are used to interact with the remote authentication
+ * service, retrieving storage system routing information and session tokens.
+ */
+class Authentication
+{
+
+    /**
+     * The information about the authentication URL.
+     *
+     * @var array
+     */
+    protected $urlInfo = array();
+
+    /**
+     * Request headers with username, API key and User-Agent.
+     *
+     * @var array
+     */
+    protected $headers = array();
+
+    /**
+     * User-Agent for request.
+     *
+     * @var string
+     */
+    protected $userAgent = null;
+
+    /**
+     * Request timeout.
+     *
+     * @var integer
+     */
+    protected $timeout = 5;
+
+    /**
+     * The class constructor.
+     *
+     * @param string  $username
+     * @param string  $apiKey
+     * @param string  $url
+     * @param string  $userAgent
+     * @param integer $timeout
+     */
+    public function __construct($username, $apiKey, $url, $userAgent, $timeout = 5)
+    {
+        $this->urlInfo   = Utils::parseUrl($url);
+        $this->userAgent = $userAgent;
+        $this->timeout   = intval($timeout);
+        $this->headers   = array(
+            'x-auth-user' => $username,
+            'x-auth-key'  => $apiKey,
+        );
+    }
+
+    /**
+     * Initiates authentication with the remote service and returns a
+     * array containing the storage system URL, CDN URL (can be empty)
+     * and session token.
+     *
+     * @return array
+     * @throws \Exception|\OpenStackStorage\Exceptions\ResponseError
+     * @throws \OpenStackStorage\Exceptions\AuthenticationError
+     * @throws \OpenStackStorage\Exceptions\AuthenticationFailed
+     * @throws \Exception
+     */
+    public function authenticate()
+    {
+        $client = new Client(array('timeout' => $this->timeout));
+        $client->setBaseURL(sprintf(
+            '%s://%s:%d/',
+            $this->urlInfo['scheme'],
+            $this->urlInfo['host'],
+            $this->urlInfo['port']
+        ));
+        $client->setUserAgent($this->userAgent);
+
+        try {
+            $response = $client->get($this->urlInfo['path'], null, $this->headers);
+        } catch (Exceptions\ResponseError $e) {
+            if (401 == $e->getCode()) {
+                // A status code of 401 indicates that the supplied credentials
+                // were not accepted by the authentication service.
+                throw new Exceptions\AuthenticationFailed();
+            }
+
+            throw $e;
+        }
+
+        $authToken = $response['headers']['x-auth-token'];
+        if (!$authToken) {
+            $authToken = $response['headers']['x-storage-token'];
+        }
+
+        $storageUrl = $response['headers']['x-storage-url'];
+        $cdnUrl     = $response['headers']['x-cdn-management-url'];
+
+        if (!($authToken && $storageUrl)) {
+            throw new Exceptions\AuthenticationError(
+                'Invalid response from the authentication service.'
+            );
+        }
+
+        return array(
+            $storageUrl,
+            $cdnUrl,
+            $authToken
+        );
+    }
+
+}
